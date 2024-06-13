64e975b79fdc3808b279ff9b17096714f7c9a34c
LeoFernandez037 <leonelfernandezuwu@gmail.com>
2024-06-13 10:04:08 -0400

    issue2s

--- src/components/Nutricion.jsx
+++ src/components/Nutricion.jsx
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from "react";
 import "./Notificacion.css";
+import Chatbot from "react-chatbot-kit";
+import config from "./configs/config.js";
+import MessageParser from "./configs/MessageParser";
+import ActionProvider from "./configs/ActionProvider";
 
 function Notificacion() {
   return (
     <div className="notifications">
       <div className="not-card">
         <h1 className="rajdhani-bold">Nutricion</h1>
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+        />
       </div>
     </div>
   );
